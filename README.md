da5b7a755ec75094c754ea17ab041fd4f7bf93ab
wangbinwww <13331036973@163.com>
2019-04-11 15:31:25 +0800

    初始建档

--- README.md
+++ README.md
@@ -1,29 +1,45 @@
 # vuelogin
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Run your tests
+
 ```
 npm run test
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Lints and fixes files
+
+```
+git init
+git add .
+git commit -m "first commit"
+git remote add origin https://github.com/wangbinwww/VuetifyLogin.git
+git push -u origin master
+```
--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,44 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <!-- <v-toolbar app dark>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Login</span>
+        <span class="font-weight-light">Lonin-Demo</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat href="https://www.iconics.com" target="_blank">
+        <span class="mr-2">关于</span>
+      </v-btn>
+    </v-toolbar>-->
+    <v-toolbar>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>{{Title}}</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat v-for="menu in menus">{{menu.name}}</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+import HelloWorld from "./components/HelloWorld";
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      //
+      Title: "用户名",
+      menus: [{ name: "login" }, { name: "logout" }, { name: "about" }]
+    };
+  }
+};
+</script>
